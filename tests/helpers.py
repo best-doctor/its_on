13011e6a2c4c7ca699aec9d233d0ca3aab9c8908
Yakov Istomin <y.istomin@bestdoctor.ru>
2019-09-20 15:51:19 +0300

    ONB-29: docstring

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,55 +1,62 @@
+from dynaconf.base import Settings
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.engine.strategies import EngineStrategy
-from typing import Dict
 
 from its_on.db_utils import parse_dsn
 from its_on.models import switches
-
-
-def setup_db(config: Dict) -> None:
-    engine = get_engine(config.DATABASE.SUPERUSER_DSN)
-
-    with engine.connect() as conn:
-        teardown_db(config)
-
-        test_db_name = parse_dsn(config.DATABASE.DSN)['database']
-
-        conn.execute('CREATE DATABASE {0}'.format(test_db_name))
-
-
-def teardown_db(config: Dict) -> None:
-    engine = get_engine(config.DATABASE.SUPERUSER_DSN)
-
-    test_db_name = parse_dsn(config.DATABASE.DSN)['database']
-
-    with engine.connect() as conn:
-        conn.execute("""
-                  SELECT pg_terminate_backend(pg_stat_activity.pid)
-                  FROM pg_stat_activity
-                  WHERE pg_stat_activity.datname = '{0}'
-                    AND pid <> pg_backend_pid();""".format(test_db_name))
-        conn.execute('DROP DATABASE IF EXISTS {0}'.format(test_db_name))
 
 
 def get_engine(dsn: str) -> EngineStrategy:
     return create_engine(dsn, isolation_level='AUTOCOMMIT')
 
 
-def create_tables(config: Dict) -> None:
+def setup_db(config: Settings) -> None:
+    """Настройка тестовой БД.
+
+    Удаляем старую тестовую базу и создаем новую.
+    """
+    engine = get_engine(config.DATABASE.SUPERUSER_DSN)
+
+    with engine.connect() as conn:
+        teardown_db(config)
+
+        test_db_name = parse_dsn(config.DATABASE.DSN)['database']
+        conn.execute('CREATE DATABASE {0}'.format(test_db_name))
+
+
+def teardown_db(config: Settings) -> None:
+    """Удаление тестовой БД."""
+    engine = get_engine(config.DATABASE.SUPERUSER_DSN)
+
+    test_db_name = parse_dsn(config.DATABASE.DSN)['database']
+    with engine.connect() as conn:
+        # Отключаем активные сессии
+        conn.execute(
+            """
+            SELECT pg_terminate_backend(pg_stat_activity.pid)
+            FROM pg_stat_activity
+            WHERE pg_stat_activity.datname = '{0}' AND pid <> pg_backend_pid();
+            """.format(test_db_name)
+        )
+        # Удаляем базу
+        conn.execute('DROP DATABASE IF EXISTS {0}'.format(test_db_name))
+
+
+def create_tables(config: Settings) -> None:
     engine = get_engine(config.DATABASE.DSN)
 
     meta = MetaData()
     meta.create_all(bind=engine, tables=[switches])
 
 
-def drop_tables(config: Dict) -> None:
+def drop_tables(config: Settings) -> None:
     engine = get_engine(config.DATABASE.DSN)
 
     meta = MetaData()
     meta.drop_all(bind=engine, tables=[switches])
 
 
-def create_sample_data(config: Dict) -> None:
+def create_sample_data(config: Settings) -> None:
     engine = get_engine(config.DATABASE.DSN)
 
     with engine.connect() as conn:
