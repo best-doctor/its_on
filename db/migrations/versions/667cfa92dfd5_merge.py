e22a980bef86a912ee4321553c420cc18bc9a4ee
mcproger <a.lomonosov@bestdoctor.ru>
2019-09-30 17:51:06 +0300

    ONB-30: Merge conflicting migrations

---
+++ db/migrations/versions/667cfa92dfd5_merge.py
@@ -0,0 +1,24 @@
+"""merge
+
+Revision ID: 667cfa92dfd5
+Revises: 6e0daa4be1f8, f5a2fe518eae
+Create Date: 2019-09-30 17:49:32.940457
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '667cfa92dfd5'
+down_revision = ('6e0daa4be1f8', 'f5a2fe518eae')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
