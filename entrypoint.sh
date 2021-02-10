d066c55e5c9df8e0b23180f0524d80d902ffaa08
Igor Perepilitsyn <Kazauwa@users.noreply.github.com>
2021-02-10 17:46:47 +0300

    run all maintenance jobs in the separate branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,17 +8,19 @@ elif [ $1 = "test" ]
   then
     exec make check
     exit $?
+elif [ $1 = "pre-run" ]
+  then
+    alembic upgrade head && \
+    rm -rf /srv/www/its_on/static && \
+    cp -r /its_on/its_on/static /srv/www/its_on
 elif [ $1 = "run" ]
   then
-    alembic upgrade head
-    rm -rf /srv/www/its_on/static
-    cp -r /its_on/its_on/static /srv/www/its_on
     exec gunicorn --bind 0.0.0.0:8081 \
                   --capture-output \
-                  --workers 3 \
                   --access-logfile - \
                   --error-logfile - \
                   --log-level info \
+                  --workers ${MICROSERVICE_N_WORKERS:-1} \
                   --worker-class aiohttp.GunicornUVLoopWebWorker \
                   its_on.main:init_gunicorn_app
     exit $?
